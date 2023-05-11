85921f5483e9182a1b759f1ca49309580c8cdeba
Barbara Frosik <bfrosik@anl.gov>
2023-05-11 13:18:48 -0500

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+
+setuptools.setup(
+      name='controller',
+      author = 'Barbara Frosik',
+      author_email = 'bfrosik@anl.gov',
+      url='https://github.com/bfrosik/controller',
+      version='1.0',
+      packages=setuptools.find_packages()
+)
